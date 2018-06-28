85324c165c9d144a29b8ba9fea58bf882752977f
Duncan Williams <duncan.williams@supercarers.com>
2018-06-28 21:43:42 +0100

    correct dir names

--- demo.sh
+++ demo.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 mkdir demo
-mkdir demo/dir1
+mkdir demo/dir-1
 echo 'this is file-1-a' >> demo/dir-1/file-1-a.txt
 echo 'this is file-1-b' >> demo/dir-1/file-1-b.txt
-mkdir demo/dir2
-mkdir demo/dir2/dir2_1
-echo 'this is file-2-1-a' >> demo/dir-2/dir-1/file-2-1-a.txt
+mkdir demo/dir-2
+mkdir demo/dir-2/dir-2-1
+echo 'this is file-2-1-a' >> demo/dir-2/dir-2-1/file-2-1-a.txt
 asciinema rec
 nvim -c ':source demo.vim'
 rm -rf demo/
